35913b56f2d527a55c10ba48d901650faf0268de
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-14 03:26:27 +0700

    Add hook with styled-components for themes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,17 +6,24 @@ import Footer from './footer';
 import PrimeForm from './PrimeForm';
 import PrimesForm from './PrimesForm';
 
-function App() {
+import { ThemeProvider } from 'styled-components';
+import {lightTheme, darkTheme, moonTheme, useDarkMode} from '../service/theme';
+import { GlobalStyle } from '../service/theme/global';
+
+const App = () => {
+  const [theme, toggleTheme] = useDarkMode();
+  // console.log('Current theme: ', theme);
+
   return (
-    <div>
-      <Header />
+    <ThemeProvider theme={theme === 'light' ? lightTheme : (theme === 'dark' ? darkTheme : moonTheme)}>    
+      <GlobalStyle />
+      <Header theme={theme} toggleTheme={toggleTheme}/>
       <main className="container-fluid text-center">
         <PrimeForm />
         <PrimesForm />
       </main>
-      <Footer />
-    </div>
+      <Footer theme={theme} toggleTheme={toggleTheme}/>
+    </ThemeProvider> 
   );
-}
-
+};
 export default App;
