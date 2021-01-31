4a78ab701a3ce5a4067f935b5408bbada8ada733
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-31 15:09:55 +0700

    Add temporary localStorage mock for later tests

--- src/__tests__/global.test.tsx
+++ src/__tests__/global.test.tsx
@@ -17,3 +17,38 @@ describe('GlobalStyle: ', () => {
   });
 
 })
+
+xit('renders with a LIGHT theme', () => {
+  // const localStorageMock = {
+  //   getItem: jest.fn(),
+  //   setItem: jest.fn(),
+  //   clear: jest.fn()
+  // };
+  // global.localStorage = localStorageMock;
+
+  // global.localStorage = {
+  //   getItem: () => undefined,
+  // };
+
+  jest.spyOn(window.localStorage.__proto__, 'setItem');
+  expect(localStorage.store).toEqual('light');
+
+});
+
+// // https://www.npmjs.com/package/jest-localstorage-mock
+// it('should save to localStorage', () => {
+//   const KEY = 'theme',
+//         VALUE = 'light';
+//   dispatch(action.update(KEY, VALUE));
+//   expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, VALUE);
+//   expect(localStorage.__STORE__[KEY]).toBe(VALUE);
+//   expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+// });
+
+
+// it('should have cleared the sessionStorage', () => {
+//   dispatch(action.reset());
+//   expect(sessionStorage.clear).toHaveBeenCalledTimes(1);
+//   expect(sessionStorage.__STORE__).toEqual({}); // check store values
+//   expect(sessionStorage.length).toBe(0); // or check length
+// });
